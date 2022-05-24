cdc7d9bcfd62b0c52604b4220f4f8cadac693423
Annu-Abhishek <kumar.abhi6939@gmail.com>
2022-05-24 19:07:05 +0530

    :construction: todo app

--- Browser/jira/localStorage.js
+++ Browser/jira/localStorage.js
@@ -1,30 +1,30 @@
-// localStorage browser ka function h ..behave like an object
+// // localStorage browser ka function h ..behave like an object
 
-//set data
- localStorage.setItem("name", "Abhishek");
- localStorage.setItem("lastName", "kumar");
- localStorage.setItem("Stream", "CSE");
- localStorage.setItem("place", "Delhi")
- localStorage.hobby = "reading";
+// //set data
+//  localStorage.setItem("name", "Abhishek");
+//  localStorage.setItem("lastName", "kumar");
+//  localStorage.setItem("Stream", "CSE");
+//  localStorage.setItem("place", "Delhi")
+//  localStorage.hobby = "reading";
 
  
-//get data
-let data = localStorage.getItem("name");
-console.log(data);
-let tata = localStorage.getItem("Stream");
-console.log(tata);
-let data1 = localStorage.name;   /* 9 & 13 th line m same chiz print hoga */
-console.log(data1);
+// //get data
+// let data = localStorage.getItem("name");
+// console.log(data);
+// let tata = localStorage.getItem("Stream");
+// console.log(tata);
+// let data1 = localStorage.name;   /* 9 & 13 th line m same chiz print hoga */
+// console.log(data1);
 
-//delete/remove data
-let a =localStorage.removeItem("name");
-delete localStorage.name;
+// //delete/remove data
+// let a =localStorage.removeItem("name");
+// delete localStorage.name;
 
 
-//length of local storage
-let l= localStorage.length;
-console.log(l);
+// //length of local storage
+// let l= localStorage.length;
+// console.log(l);
 
-let d =localStorage.key(2);
-console.log(d);
+// let d =localStorage.key(2);
+// console.log(d);
 
--- Browser/jira/script.js
+++ Browser/jira/script.js
@@ -1,3 +1,4 @@
+var uid = new ShortUniqueId();
 const addBtn= document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
 const allPriorityColors = document.querySelectorAll(".priority-color");
@@ -10,20 +11,21 @@ let ticketsArr= [];
 let toolBoxColors = document.querySelectorAll(".color");
 let removeBtn = document.querySelector(".remove-btn");
 
+//to open and close modal container
  let isModalPresent = false;
- addBtn.addEventListener('click', function(){
+ addBtn.addEventListener("click", function(){
      if(!isModalPresent){
          modalCont.style.display ="flex";    //modal add ho gya//
-         
+        //  isModalPresent = true;
      }else{
          modalCont.style.display="none";   
-         
+        //  isModalPresent = false;
      }
      isModalPresent = !isModalPresent;           //toggling effect....agr true h to false kr do and vice-versa//
  });
 
  
-
+// to remove and add active class from each priority color of modal container 
  allPriorityColors.forEach(function(colorElement){
      colorElement.addEventListener("click", function (){
          allPriorityColors.forEach(function(priorityColorElem){
@@ -34,6 +36,7 @@ let removeBtn = document.querySelector(".remove-btn");
      });
  });
 
+ //to generate and display a ticket
  modalCont.addEventListener("keydown", function(e){
      let key =e.key;
      if (key == "Shift"){
@@ -49,14 +52,74 @@ let removeBtn = document.querySelector(".remove-btn");
      }
  });
 
- function createTicket(ticketColor, data){                  /* JS ki help s HTML likha ja rha h*/
+ //function to create new tickets
+ function createTicket(ticketColor, data, ticketId){
+    let id = ticketId || uid();     
+    
+    /* JS ki help s HTML likha ja rha h*/
      let ticketCont = document.createElement("div");     
      ticketCont.setAttribute("class", "ticket-cont");
      ticketCont.innerHTML = `
      <div class = "ticket-color ${ticketColor}"></div>
-     <div class = "ticket-id"></div>
+     <div class = "ticket-id">${id}</div>
+     
      <div class = "task-area">${data}</div>
      `;
      mainCont.appendChild(ticketCont);
 
- }
+
+
+if(!ticketId){
+    ticketsArr.push({ticketColor, data, ticketId: id});
+    localStorage.setItem("tickets", JSON.stringify(ticketsArr));
+   }
+
+};
+
+//get all tickets from local storage
+if(localStorage.getItem("tickets")){
+    ticketsArr = JSON.parse(localStorage.getItem("tickets"));
+    ticketsArr.forEach(function(ticketObj){
+        createTicket(ticketObj.ticketColor, ticketObj.data, ticketObj.ticketId);
+    })
+}
+
+//filter tickets on the basis of ticketcolor
+for(let i= 0; i<toolBoxColors.length;i++){
+    toolBoxColors[i].addEventListener("click", function(){
+        let currToolBoxColor = toolBoxColors[i].classList[0];
+
+        let filteredTickets = ticketsArr.filter(function(ticketObj){
+            if (currToolBoxColor == ticketObj.ticketColor) {
+                return ticketObj;
+            }
+        });
+
+        // remove all tickets
+        let allTickets = document.querySelectorAll(".ticket-cont");
+        for(let i=0; i<allTickets.length; i++){
+            allTickets[i].remove();
+        }
+
+        //display filteredTickets
+        filteredTickets.forEach(function (ticketObj){
+            createTicket(ticketObj.ticketColor, ticketObj.data, ticketObj.ticketId);
+        })
+    })
+
+    // to display all the tickets of all colors on double clicking
+    toolBoxColors[i].addEventListener("dblclick", function(){
+
+        //remove all the color specific tickets
+        let allTickets = document.querySelectorAll(".ticket-cont");
+        for(let i=0; i<allTickets.length;i++){
+            allTickets[i].remove();
+        }
+
+        // display all Tickets
+        ticketsArr.forEach(function(ticketObj){
+           createTicket (ticketObj.ticketColor, ticketObj.data, ticketObj.ticketId)
+        }) ;
+    })
+}
+
